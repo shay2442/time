150074c5005aea47b3165a76da89ab03e5434459
Shay <shay2442@yahoo.com>
2022-10-05 08:06:14 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [timerOn, setTimerOn] = useState(true)
+
+  useEffect(() => {
+    <div>{time}</div>
+    let interval = null
+    if(timerOn) {
+     interval = setInterval(() => {
+       setTime(prevTime => prevTime + 10)
+     },10)
+
+    }else {
+      clearInterval(interval)
+    }
+    return () => clearInterval(interval)
+
+  },[timerOn])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <span>{("0" + Math.floor((time /60000) % 60)).slice(-2)}</span><br/>
+    <button onClick={() => setTimerOn(true)}>Start</button>
+    <button onClick={() => setTimerOn(false)}>Stop</button>
+    <button onClick={() => setTime(0)}>Reset</button>
     </div>
   );
 }

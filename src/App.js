1bbb1539c9e6a0cb2be3a89eb2ccf520df3440d9
Shay <shay2442@yahoo.com>
2022-10-28 09:24:41 -0500

    Added songs and making calls to song-api

--- src/App.js
+++ src/App.js
@@ -1,55 +1,81 @@
-import {useState, useEffect} from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-const [pokemons, setPokemons] = useState([])
-const [pokemon, setPokemon] = useState('')
-const [currentPokemon, setCurrentPokemon] = useState(1)
-const [time, setTime] = useState(0)
-const [timerOn, setTimerOn] = useState(true)
-const [likes, setLikes] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [pokemon, setPokemon] = useState("");
+  const [currentPokemon, setCurrentPokemon] = useState(18);
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(true);
+  const [likes, setLikes] = useState(0);
+  const [songs, setSongs] = useState([]);
+  const [song, setSong] = useState("");
+  const [currentSong, setCurrentSong] = useState(18);
 
-useEffect(() => {
-  let interval = null;
-  if(timerOn) {
-    interval = setInterval(() => {setTime(prevTime => prevTime + 1)},100)
-  }else {
-  clearInterval(interval)
+  useEffect(() => {
+    fetch(`http://localhost:3001/songs/${currentSong}`)
+      .then((r) => r.json())
+      .then((song) => setSong(song));
+  },[]);
+
+  // useEffect(() => {
+  //   let interval = null;
+  //   if(timerOn) {
+  //     interval = setInterval(() => {setTime(prevTime => prevTime + 1)},100)
+  //   }else {
+  //   clearInterval(interval)
+  //   }
+  //   return () => clearInterval(interval)
+
+  // },[timerOn])
+
+  useEffect((name) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`)
+      .then((r) => r.json())
+      .then((pokemon) => setPokemon(pokemon));
+  });
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/songs')
+  //   .then((r) => r.json())
+  //   .then((songs) => setSongs(songs))
+  // },[])
+
+  function nextPokemon() {
+    setCurrentPokemon(currentPokemon + 1);
   }
-  return () => clearInterval(interval)
-
-},[timerOn])
 
 
-useEffect((name) => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemon}`,{})
-  .then((r) => r.json())
-  .then((pokemon) => setPokemon(pokemon))
-})
-
-function nextPokemon() {
-  setCurrentPokemon(currentPokemon + 1)
-}
 
   return (
     <div className="App">
+      {pokemon ? <img src={pokemon.sprites.front_default} /> : "No Pokemon Yet"}
+      <h1>{pokemon.name}</h1>
+      <button onClick={nextPokemon}>Next Pokemon</button>
+      <br></br>
+      {likes}
+      <button onClick={() => setLikes(likes + 1)}>Like</button>
+      <br></br>
+      {/* {songs.map((song) => {
+        return (
+          <h3>
+            {song.title}, {song.genre}, ${song.price},{" "}
+            <img height="200" width="280" src={song.cover} />
+          </h3>
+        );
+      })} */}
 
-{pokemon ? <img src={pokemon.sprites.front_default}/> : "No Pokemon Yet"}
-<h1>{pokemon.name}</h1>
-<button onClick={nextPokemon}>Next Pokemon</button>
-<br></br>
-{likes}
-<br></br>
-<br></br>
-{time}
+      <h1>{song ? <img height="100" width="180"src={song.cover}/> : "No song to show"}</h1>
+      <h1>{song.title}, {song.genre},${song.price}</h1>
+      <br></br>
+      <button onClick={() => setCurrentSong(currentSong + 1)}>Get Random Song</button>
+      {/* {time}
 <br></br>
 <button onClick={() => setTimerOn(true)}>Start</button>
 <button onClick={() => setTimerOn(false)}>Stop</button>
-<button onClick={() => setTime(0)}>Reset</button>
+<button onClick={() => setTime(0)}>Reset</button> */}
 
-
-
-     {/* {pokemons.map((pokemon) => {
+      {/* {pokemons.map((pokemon) => {
        return <h1>{pokemon.name}</h1>
      })} */}
     </div>
